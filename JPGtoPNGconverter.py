f5c8799268f569cc5eb02f053a7a3b6020fd5b0e
sif27000 <43202235+sif27000@users.noreply.github.com>
2020-04-16 15:10:19 +0200

    Add files via upload

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,12 +2,17 @@ import sys
 import os
 from PIL import Image
 
+# grabs arguments from console
 path = sys.argv[1]
 directory = sys.argv[2]
 
+# check if directory exists, if not create it
 if not os.path.exists(directory):
     os.makedirs(directory)
 
+# eterate filenames in the gives source directory
+# opens each files and save it in the new directory
+# converting JPG to PGN
 for filename in os.listdir(path):
   clean_name = os.path.splitext(filename)[0]
   img = Image.open(f'{path}{filename}')
